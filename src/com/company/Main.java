f20a4ab77f03b305f4e1b9491b6b3eb37e81d15f
JaewonMin <mjw8523@gmail.com>
2020-12-28 18:25:13 +0900

    jw commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("초기상태입니다");
+        jaewonClass jwClass = new jaewonClass();
     }
+
 }
+
+
