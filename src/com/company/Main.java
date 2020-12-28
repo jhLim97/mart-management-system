3fcc8603a51bc7c2fafc830f019c582e4e51ec9c
방규빈 <rbqls1057@naver.com>
2020-12-28 18:19:35 +0900

    Modified by gyubin

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("초기상태입니다");
+        GyubinClass gyubin = new GyubinClass();
     }
 }
