b739a0dcbe32d1c367d26187521d83d4a2b2ada1
jhLim97 <wnsgur2749@naver.com>
2020-12-30 18:07:41 +0900

    review 수정

--- src/com/company/View/CustomerViewPanel.java
+++ src/com/company/View/CustomerViewPanel.java
@@ -3,100 +3,95 @@ package com.company.View;
 import javax.swing.*;
 import java.awt.*;
 
-public class CustomerView extends JPanel {
+public class CustomerViewPanel extends JPanel {
 
-    private JPanel customerPanel, optionPanel, customerListPanel, cusInfoPanel, newCustomerPanel;
+    private JPanel optionPanel, cusInfoPanel, newCustomerPanel;
     private JTextField jtfPhoneNum;
     private JButton btnSearch, btnCustomerMange;
     private JTextArea jtaCustomerList, jtaNewCustomer;
     private JLabel lblPhoneNum, lblName, lblPoint, lblNewCustomer;
     private JScrollPane jsp;
 
-    public CustomerView() {
+    public CustomerViewPanel() {
+
+        // -------optionPanel--------
+        optionPanel = new JPanel(); // 상단 조회 및 관리 패널
+
+        jtfPhoneNum = new JTextField(15);
+        btnSearch = new JButton("조회");
+        btnCustomerMange = new JButton("고객 정보 관리");
+
+        // ------- cusInfoPanel -------
+        cusInfoPanel = new JPanel(); // 고객 칼럼 라벨들 add 할 패널
+
+        lblPhoneNum = new JLabel("휴대폰 번호");
+        lblName = new JLabel("고객 이름");
+        lblPoint = new JLabel("누적 포인트");
+
+        // ------- jtaCustomerList -------
+        jtaCustomerList = new JTextArea(" ", 10, 30); // 고객 정보 리스팅 하는 Area
+        jsp = new JScrollPane(jtaCustomerList, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+
+        // -------newCustomerPanel-------
+        newCustomerPanel = new JPanel();
+        lblNewCustomer = new JLabel("신규 고객");
+        jtaNewCustomer = new JTextArea(" ", 5, 5);
+
+        drawView();
+
+    }
+
+    public void drawView() {
+
         //setSize(500, 300);
         setBounds(0, 30, 1200, 735);
         setPreferredSize(new Dimension(1200, 735));
         setLayout(null);
 
-        // -------customerPanel-------
-        //customerPanel = new JPanel(); // 세 개의 패널을 add할 커스터머 전체 패널
-        //customerPanel.setLayout(new BorderLayout());
-
-        // -------optionPanel--------
-        optionPanel = new JPanel(); // 상단 조회 및 관리 패널
+        // ------- optionPanel --------
         optionPanel.setBounds(0, 0, 1200, 30);
         optionPanel.setLayout(null);
 
-        jtfPhoneNum = new JTextField(15);
         jtfPhoneNum.setBounds(20, 5, 100, 20);
-
-        btnSearch = new JButton("조회");
         btnSearch.setBounds(140, 5, 60, 20);
-
-        btnCustomerMange = new JButton("고객 정보 관리");
         btnCustomerMange.setBounds(220, 5, 130, 20);
 
-        // -------customerListPanel-------
-        //customerListPanel = new JPanel();
-        //customerListPanel.setBounds(0, 35, 1200, 30);
-        //customerListPanel.setLayout(null);
-
-        cusInfoPanel = new JPanel(); // 고객 칼럼 라벨들 add 할 패널
-        cusInfoPanel.setBounds(0, 35, 1000, 70);
-        cusInfoPanel.setLayout(null);
-
-        lblPhoneNum = new JLabel("휴대폰 번호");
-        lblPhoneNum.setBounds(130, 30, 150, 50);
-
-        lblName = new JLabel("고객 이름");
-        lblName.setBounds(320, 30, 150, 50);
-
-        lblPoint = new JLabel("누적 포인트");
-        lblPoint.setBounds(510, 30, 150, 50);
-
-        jtaCustomerList = new JTextArea(" ", 10, 30); // 고객 정보 리스팅 하는 Area
-        jtaCustomerList.setEditable(false);
-
-        jsp = new JScrollPane(jtaCustomerList, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        jsp.setBounds(100, 120, 800, 600);
-
-        // -------newCustomerPanel-------
-        newCustomerPanel = new JPanel();
-        newCustomerPanel.setBounds(950, 120, 200, 600);
-        newCustomerPanel.setLayout(null);
-
-        lblNewCustomer = new JLabel("신규 고객");
-        lblNewCustomer.setBounds(20, 50, 160, 50);
-
-        jtaNewCustomer = new JTextArea(" ", 5, 5);
-        jtaNewCustomer.setBounds(20, 120, 160, 400);
-
-        drawView();
-
-        setVisible(true);
-
-    }
-
-    public void drawView() {
         optionPanel.add(jtfPhoneNum);
         optionPanel.add(btnSearch);
         optionPanel.add(btnCustomerMange);
+
+        // ------- cusInfoPanel -------
+        cusInfoPanel.setBounds(0, 35, 1000, 70);
+        cusInfoPanel.setLayout(null);
+
+        lblPhoneNum.setBounds(130, 30, 150, 50);
+        lblName.setBounds(320, 30, 150, 50);
+        lblPoint.setBounds(510, 30, 150, 50);
 
         cusInfoPanel.add(lblPhoneNum);
         cusInfoPanel.add(lblName);
         cusInfoPanel.add(lblPoint);
 
-        //customerListPanel.add(cusInfoPanel);
-        //customerListPanel.add(jsp);
+        // ------- jtaCustomerList -------
+        jtaCustomerList.setEditable(false);
+        jsp.setBounds(100, 120, 800, 600);
+
+        // ------- newCustomerPanel -------
+        newCustomerPanel.setBounds(950, 120, 200, 600);
+        newCustomerPanel.setLayout(null);
+
+        lblNewCustomer.setBounds(20, 50, 160, 50);
+        jtaNewCustomer.setBounds(20, 120, 160, 400);
 
         newCustomerPanel.add(lblNewCustomer);
         newCustomerPanel.add(jtaNewCustomer);
 
         add(optionPanel);
-        //add(customerListPanel);
         add(cusInfoPanel);
         add(jsp);
         add(newCustomerPanel);
+
+        setVisible(true);
     }
 
 }
--- src/com/company/View/ShoppingView.java
+++ src/com/company/View/ShoppingView.java
@@ -3,7 +3,7 @@ package com.company.View;
 import javax.swing.*;
 import java.awt.*;
 
-public class ShoppingFrame extends JFrame {
+public class ShoppingView extends JFrame {
 
     //private JPanel selectProductPanel, productListPanel, myShoppingPanel;
     private JLabel lblProduct, lblSearch, lblCount, lblItemList, lblMyList, lblMsg;
@@ -12,46 +12,27 @@ public class ShoppingFrame extends JFrame {
     private JTable tbItem, tbMyList;
     private JScrollPane jspItem, jspMyList;
     private JButton btnDelete1, btnDelete2, btnPay;
+    private Font fnt, fntDelete;
 
-    public ShoppingFrame() {
-
-        super("Shopping View");
-        setSize(400, 520);
-        setLayout(null);
-        setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+    public ShoppingView() {
 
         // 폰트 설정
-        Font fnt = new Font("Dialog", Font.BOLD, 15);
-        Font fntDelete = new Font("Dialog", Font.BOLD, 10);
+        fnt = new Font("Dialog", Font.BOLD, 15);
+        fntDelete = new Font("Dialog", Font.BOLD, 10);
 
         // -----------label, textfield-----------
         lblProduct = new JLabel("상품");
-        lblProduct.setFont(fnt);
-        lblProduct.setBounds(20, 20, 60, 40);
-
         lblSearch = new JLabel("검색");
-        lblSearch.setFont(fnt);
-        lblSearch.setBounds(20, 70, 60, 40);
-
         lblCount = new JLabel("수량");
-        lblCount.setFont(fnt);
-        lblCount.setBounds(210, 70, 60, 40);
 
         cb = new JComboBox(); // 상품을 콤보박스에서 조회할 수 있도록 생성
-        cb.setBounds(90, 25, 80, 30);
-
         jtfSearch = new JTextField();
-        jtfSearch.setBounds(90, 75, 100, 30);
-
         jtfCount = new JTextField();
-        jtfCount.setBounds(280, 75, 70, 30);
 
         // -----------ItemList-----------
         lblItemList = new JLabel("구매 할 수 있는 물품 List");
-        lblItemList.setFont(fnt);
-        lblItemList.setBounds(20, 115, 200, 30);
 
-        // table에 들어갈 rows, columns 정의
+        // table에 들어갈 rows, columns 생성
         String Itemcolumns[]={"No.", "A", "B", "C", "D", "E"};
         String Itemcontents[][]={
                 {"1", " ", " ", " ", " ", " "},
@@ -68,12 +49,9 @@ public class ShoppingFrame extends JFrame {
         // table 생성
         tbItem = new JTable(Itemcontents, Itemcolumns);
         jspItem = new JScrollPane(tbItem, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        jspItem.setBounds(10, 150, 360, 160);
 
         // -----------MyList-----------
         lblMyList = new JLabel("내가 담은 List(장바구니)");
-        lblMyList.setFont(fnt);
-        lblMyList.setBounds(20, 315, 200, 30);
 
         // table에 들어갈 rows, columns 정의
         String MyListcolumns[]={"No.", "A", "B", "C", "수량"};
@@ -92,54 +70,76 @@ public class ShoppingFrame extends JFrame {
         // table 생성
         tbMyList = new JTable(MyListcontents, MyListcolumns);
         jspMyList = new JScrollPane(tbMyList, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        jspMyList.setBounds(10, 350, 310, 70);
 
         btnDelete1 = new JButton("삭제");
-        lblMyList.setFont(fntDelete);
-        btnDelete1.setBounds(320, 373, 60, 15);
-
         btnDelete2 = new JButton("삭제");
-        lblMyList.setFont(fntDelete);
-        btnDelete2.setBounds(320, 388, 60, 15);
 
         // -----------PayMsg-----------
         lblMsg = new JLabel("결제 금액 : 75,000원");
-        lblMsg.setFont(fnt);
-        lblMsg.setBounds(30, 420, 150, 60);
-
         btnPay = new JButton("결제하기");
-        lblMyList.setFont(fnt);
-        btnPay.setBounds(285, 435, 90, 30);
 
         drawView();
     }
 
     public void drawView() {
 
+        setTitle("Shopping View");
+        setSize(400, 520);
+        setLayout(null);
+        setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+
         // -----------label, textfield add-----------
+        lblProduct.setFont(fnt);
+        lblProduct.setBounds(20, 20, 60, 40);
         add(lblProduct);
+
+        lblSearch.setFont(fnt);
+        lblSearch.setBounds(20, 70, 60, 40);
         add(lblSearch);
+
+        lblCount.setFont(fnt);
+        lblCount.setBounds(210, 70, 60, 40);
         add(lblCount);
 
+        cb.setBounds(90, 25, 80, 30);
         add(cb);
+
+        jtfSearch.setBounds(90, 75, 100, 30);
         add(jtfSearch);
+
+        jtfCount.setBounds(280, 75, 70, 30);
         add(jtfCount);
+
+        lblItemList.setFont(fnt);
+        lblItemList.setBounds(20, 115, 200, 30);
         add(lblItemList);
 
         // -----------ItemList add-----------
+        jspItem.setBounds(10, 150, 360, 160);
         add(jspItem);
 
         // -----------MyList-----------
+        lblMyList.setFont(fnt);
+        lblMyList.setBounds(20, 315, 200, 30);
         add(lblMyList);
 
+        jspMyList.setBounds(10, 350, 310, 70);
         add(jspMyList);
 
+        btnDelete1.setFont(fntDelete);
+        btnDelete1.setBounds(320, 373, 60, 15);
         add(btnDelete1);
+
+        btnDelete2.setFont(fntDelete);
+        btnDelete2.setBounds(320, 388, 60, 15);
         add(btnDelete2);
 
         // -----------PayMsg-----------
+        lblMsg.setFont(fnt);
+        lblMsg.setBounds(30, 420, 150, 60);
         add(lblMsg);
 
+        btnPay.setBounds(285, 435, 90, 30);
         add(btnPay);
 
         //pack();
@@ -147,7 +147,7 @@ public class ShoppingFrame extends JFrame {
     }
 
     public static void main(String[] args) {
-        new ShoppingFrame();
+        new ShoppingView();
     }
 
 }
