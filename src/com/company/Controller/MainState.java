32a9d22d85e6273a8b6a61e9a6eb4cda40b6e999
JaewonMin <mjw8523@gmail.com>
2021-01-01 00:50:55 +0900

    충돌 처리 완료

--- src/com/company/Controller/MainState.java
+++ src/com/company/Controller/MainState.java
@@ -1,22 +1,11 @@
 package com.company.Controller;
 
-<<<<<<< HEAD
-=======
-import com.company.Main;
->>>>>>> 17264d35ecf291b28383368a0fefa28424ce7abc
 import com.company.View.MainView;
 import com.company.View.ProductViewPanel;
 
 import javax.swing.*;
-<<<<<<< HEAD
+
 import java.sql.SQLException;
-
-public class MainState implements State{
-
-    MainView productView;
-    ProductViewPanel productPanel;
-
-=======
 
 public class MainState implements State{
 
@@ -27,29 +16,18 @@ public class MainState implements State{
 
 
     }
->>>>>>> 17264d35ecf291b28383368a0fefa28424ce7abc
     @Override
     public void drawFrame() {
 
     }
 
-    @Override
-<<<<<<< HEAD
     public void drawPanel() throws SQLException, ClassNotFoundException {
         ProgramManager.getInstance().getMainView().drawMainPanel();
         ProgramManager.getInstance().getMainView().drawProductViewPanel();
-=======
-    public void drawPanel() {
-//        productView.drawMainPanel();
-//        productView.drawProductViewPanel();
->>>>>>> 17264d35ecf291b28383368a0fefa28424ce7abc
-
     }
 
     @Override
     public void applyListener() {
-<<<<<<< HEAD
-=======
         productView = ProgramManager.getInstance().getMainView();
         productView.productButton.addActionListener(e -> {
 
@@ -64,7 +42,6 @@ public class MainState implements State{
             ProgramManager.getInstance().setCustomerManageState();
 
         });
->>>>>>> 17264d35ecf291b28383368a0fefa28424ce7abc
 
     }
 
@@ -72,8 +49,6 @@ public class MainState implements State{
     public void update() {
 
     }
-<<<<<<< HEAD
-=======
 //    public static void main(String[] args) {
 //        MainState state = new MainState();
 //        ProgramManager.getInstance().setState(state);
@@ -82,5 +57,4 @@ public class MainState implements State{
 //        ProgramManager.getInstance().getMainView().drawProductViewPanel();
 //        state.applyListener();
 //    }
->>>>>>> 17264d35ecf291b28383368a0fefa28424ce7abc
 }
