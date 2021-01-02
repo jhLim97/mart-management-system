ed1c0fa03331ef4c68f0dea48aee8f593e6501e2
ChangMin_ <dlwlxns4@gmail.com>
2021-01-02 18:24:39 +0900

    no message

--- src/com/company/Server/ChangMinServer.java
+++ src/com/company/Server/ChangMinServer.java
@@ -1,49 +1,107 @@
 package com.company.Server;
 
+import com.google.gson.Gson;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
-public class ChangMinServer {
 
-    private Logger logger;
+public class ChangminServer {
 
-    // Message 객체를 json 객체로 파싱하기 위한 Gson 객체 생성
-    private Gson gson = new Gson();
+    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------
+    static final int login = 1; // 여기에 타입 번호 맵핑한 후 임의로 작성 한 후 주석으로 남기기
+    static final int update = 13;
+    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------
 
-    private Socket s;
+    private ServerSocket ss = null;
+    private Socket s = null; // client를 받기 위한 매개체
+    private boolean status = true;
 
-    // 입출력 스트림
-    private BufferedReader inMsg = null; // 따로 설정 안해도 되나?
-    private PrintWriter outMsg = null;
+    // 연결된 client 스레드를 관리하는 ArrayList
+    ArrayList<MMSThread> mmsThreadList = new ArrayList<MMSThread>();
 
-    private boolean status;
+    Logger logger; // 로거 객체 선언
 
-    private Message m;
+    public void start(){
+        logger = Logger.getLogger(this.getClass().getName());
 
-    private Thread thread;
-
-
-    public void connectServer() {
         try {
-            s = new Socket("127.0.0.1", 7777);
-            logger.log(Level.INFO, "[Client]Server 연결 성공!!");
-            inMsg = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            outMsg = new PrintWriter(s.getOutputStream(), true);
+            ss = new ServerSocket(7777); // 임의의 포트번호를 통해 서버 소켓 생성
+            logger.info("MultiChatServer start");
 
-            m = new Message(v.id, "", "", "login");
-            outMsg.println(gson.toJson(m));
+            while (true) {
+                s = ss.accept();
 
-            thread = new Thread(this);
-            thread.start();
+                MMSThread chat = new MMSThread();
+                mmsThreadList.add(chat);
+                MMSThread.start();
+            }
 
         } catch (Exception e) {
-            logger.log(Level.WARNING, "[MultiChatUI]connectServer() Exception 발생!!");
+            logger.info("[MultiChatServer]start() Exception 발생!!");
             e.printStackTrace();
         }
     }
 
+
+    class MMSThread extends Thread {
+
+        String msg;
+
+        Message m = new Message();
+
+        Gson gson = new Gson(); // Message 객체를 json 객체로 파싱하기 위한 Gson 객체 생성
+
+        // 입출력 스트림
+        private BufferedReader inMsg = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        private PrintWriter outMsg = new PrintWriter(s.getOutputStream(), true);
+
+        public String id = null;
+
+        public MMSThread() throws IOException {
+        }
+
+        public void run() {
+            while (status) { // 상태 정보가 true 일경우 반복문을 돌며 수신된 메세지 처리
+                try {
+                    msg = inMsg.readLine(); // 메세지 수신
+                    //m = gson.fromJson(msg, Message.class); // Message 클래스로 매핑
+
+                    switch(m.getType()) {
+                        case login : break;
+
+
+                        case update : break;
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    status = false;
+                }
+
+            }
+            this.interrupt();
+            logger.info(this.getName() + " 종료됨!!");
+        }
+
+        public void msgSendAll(String msg) {
+            for (MMSThread ct : mmsThreadList) {
+                ct.outMsg.println(msg);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        ChangminServer changminServer = new ChangminServer();
+        ChangminServer.start();
+    }
 }
+
